4d662ade444020a33318bc04984407b72d1f6e22
Chad Linthicum <portableanswers@gmail.com>
2024-08-21 18:29:45 -0400

    -Added constact button to mobile nav -Set desktop nav to fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,7 @@
       '<span class="title">' +
       $("#logo").html() +
       "</span>" +
+      '<a id="nav-contact" href="#contact" class="button primary scrolly">☎ Contact</a>' +
       "</div>"
   ).appendTo($body);
 
